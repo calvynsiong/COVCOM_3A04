4f84131633124ae8605482b02118915ad625390d
Lukas Vadeika <vadeikal@mcmaster.ca>
2024-04-06 21:36:13 -0400

    Maintain id in userdata

--- app/src/main/java/com/example/covcom/Entity/User.java
+++ app/src/main/java/com/example/covcom/Entity/User.java
@@ -3,5 +3,5 @@ package com.example.covcom.Entity;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public String name, password, fakepassword, email, gender, phonenum, timezone;
+    public String name, id, password, fakepassword, email, gender, phonenum, timezone;
 }
