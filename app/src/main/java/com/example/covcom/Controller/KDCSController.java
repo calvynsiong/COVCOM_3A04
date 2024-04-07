15f42f199ef143176795ae23a6f8c90217ab2964
tammmyz <tammy.zeng908@gmail.com>
2024-04-07 01:21:10 -0400

    Create method to create session key

--- app/src/main/java/com/example/covcom/Controller/KDCSController.java
+++ app/src/main/java/com/example/covcom/Controller/KDCSController.java
@@ -1,38 +1,56 @@
 package com.example.covcom.Controller;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.spec.SecretKeySpec;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.covcom.Constants;
+
+import javax.crypto.*;
+//import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.util.Base64;
 public class KDCSController {
-    private static String algorithm = "AES";
-    private static HashMap<String, Key> keystore = null;
+    private String user;
+    private static final String algorithm = "AES";
+    private FirebaseFirestore database;
+    private static final String TAG = "KDCS";
 
-    public KDCSController() {
-
+    // Initialize KDCS for currently authenticated user
+    public KDCSController(String user) {
+        this.user = user;
+        this.database = FirebaseFirestore.getInstance();
+        // bonus: add a master key for encryption before storing on Firebase
     }
 
-    static{
-        try
-        {
-            keystore = new HashMap<>();
-            Key ka = generateKey();
-            Key kb = generateKey();
-
-            keystore.put("Alice", ka);
-            keystore.put("bob", kb);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Key generateKey() throws Exception{
-
+    // Create symmetric session keys for both chat users and store in Firestore
+    // for specific chat session
+    public void generateSessionKey(String recipient) throws Exception{
         SecretKey k =  KeyGenerator.getInstance(algorithm).generateKey();
-        return new SecretKeySpec(k.getEncoded(), 0, 16, algorithm);
+        String keyStr = Base64.getEncoder().encodeToString(k.getEncoded());;
+        addSessionKeyToDB(this.user, recipient, keyStr);
+        addSessionKeyToDB(recipient, this.user, keyStr);
     }
 
-    public static Key lookup(String alias)
-    {
-        return (keystore!=null) ? keystore.get(alias) : null;
+    // Update Firestore session key for user
+    private void addSessionKeyToDB(String sender, String recipient, String key) {
+        String sessionKeyId = String.format("%s:%s", sender, recipient);
+        this.database.collection(Constants.KDCS).document(sessionKeyId)
+                .set(key)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Session key successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating session key", e);
+                    }
+                });
     }
 }
