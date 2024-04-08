c0a4c5e2ff8520adecc2bc4d91a02df5fd918ed8
Lukas Vadeika <vadeikal@mcmaster.ca>
2024-04-07 22:46:03 -0400

    Create Modify Layout and create initial controller class

--- app/src/main/java/com/example/covcom/Controller/ManageAccountController.java
+++ app/src/main/java/com/example/covcom/Controller/ManageAccountController.java
@@ -1,6 +1,31 @@
 package com.example.covcom.Controller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.covcom.Constants;
+import com.example.covcom.databinding.ActivityManageBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ManageAccountController extends AppCompatActivity {
+    private SharedPreferences preferences;
+    private FirebaseFirestore database;
+    private ActivityManageBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityManageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        preferences = getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCE_KEY, Context.MODE_PRIVATE);
+
+
+    }
 }
