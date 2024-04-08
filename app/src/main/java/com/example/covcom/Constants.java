6326594c30204bbf2f8717671e92c72532dba566
tammmyz <tammy.zeng908@gmail.com>
2024-04-07 22:16:40 -0400

    Update generate session key to add key if does not already exist

--- app/src/main/java/com/example/covcom/Constants.java
+++ app/src/main/java/com/example/covcom/Constants.java
@@ -4,9 +4,8 @@ public class Constants {
     public static final String DATABASE_USERS = "users";
     public static final String DATABASE_USERNAME = "username";
     public static final String DATABASE_PASSWORD = "password";
+    public static final String DATABASE_KDCS = "kdc";
     public static final String SHARED_PREFERENCE_KEY = "preferences";
-    public static final String KDCS = "kdc";
-
     public static final String KEY_USER = "user";
 
     public static final String KEY_USER_ID = "userId";
